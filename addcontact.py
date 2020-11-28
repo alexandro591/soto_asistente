2a6309da7c7d3192f09ef1deb16f7429b607e716
Alexandro <alexandro.tapia@devsu.com>
2020-11-28 09:26:25 -0500

    UPDATE

--- addcontact.py
+++ addcontact.py
@@ -22,18 +22,46 @@ except:
 
 logo = logo.resize((120,120))
 
-entry_name = Label(app, text="Nombre:", font=("Helvetica", 12), fg = "white")
-entry_name.configure(anchor="center", background='black')
-entry_name.place(x=200, y=250)
-
-
 render = ImageTk.PhotoImage(logo)
 
 logo_label = Label(app, image=render)
 logo_label.configure(background='black')
 logo_label.image = render
 logo_label.place(x=W/2-55, y=80)
+
 ######################################
+entry_name = Label(app, text="Nombre", font=("Helvetica", 12), fg = "white")
+entry_name.configure(anchor="center", background='black')
+entry_name.place(x=200, y=200)
+
+name = Entry(app)
+name.place(x=200, y=220)
+######################################
+
+######################################
+entry_name = Label(app, text="Teléfono", font=("Helvetica", 12), fg = "white")
+entry_name.configure(anchor="center", background='black')
+entry_name.place(x=200, y=250)
+
+phone = Entry(app)
+phone.place(x=200, y=270)
+######################################
+
+######################################
+entry_name = Label(app, text="Correo", font=("Helvetica", 12), fg = "white")
+entry_name.configure(anchor="center", background='black')
+entry_name.place(x=200, y=300)
+
+email = Entry(app)
+email.place(x=200, y=320)
+######################################
+
+###################################### TECLADO ####################################
+
+Q = Button(app, text = "Q")
+Q.place(x=600, y=220)
+W = Button(app, text = "W")
+W.place(x=620, y=220)
 
 app.mainloop()
 
