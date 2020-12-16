dd3fd27ed75878ba0755e63a260c53e460c83980
Alexandro <alexandro.tapia@devsu.com>
2020-12-16 00:19:49 -0500

    update

---
+++ faces.py
@@ -0,0 +1,17 @@
+import subprocess
+from tkinter import *
+from PIL import Image, ImageTk
+from os import path
+
+W = 150
+H = 80
+
+root = Tk()
+try:
+    img = ImageTk.PhotoImage(Image.open("faces/normal.png").resize((W, H), Image.ANTIALIAS))
+except:
+    img = ImageTk.PhotoImage(Image.open("/home/pi/soto_hmi/faces/normal.png").resize((W, H), Image.ANTIALIAS))
+
+panel = Label(root, image = img)
+panel.pack()
+root.mainloop()
